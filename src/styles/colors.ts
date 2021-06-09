6534e9751ea00131aeaf578b842a93ad744f35df
John Haupenthal <john.haupenthal@vincit.com>
2021-06-08 21:10:40 -0700

    Add colors.

---
+++ src/styles/colors.ts
@@ -0,0 +1,46 @@
+// https://flatuicolors.com/palette/us
+
+export type Color =
+  | 'lightGreenishBlue'
+  | 'mintLeaf'
+  | 'sourLemon'
+  | 'brightYarrow'
+  | 'fadedPoster'
+  | 'robinsEggBlue'
+  | 'firstDate'
+  | 'orangeVille'
+  | 'greenDarnerTail'
+  | 'electronBlue'
+  | 'pinkGlamour'
+  | 'chiGong'
+  | 'shyMoment'
+  | 'exodusFruit'
+  | 'pico8Pink'
+  | 'prunusAvium'
+  | 'cityLights'
+  | 'soothingBreeze'
+  | 'americanRiver'
+  | 'draculaOrchid';
+
+export const colors: { [key in Color]: string } = {
+  lightGreenishBlue: '#55efc4',
+  mintLeaf: '#00b894',
+  sourLemon: '#ffeaa7',
+  brightYarrow: '#fdcb6e',
+  fadedPoster: '#81ecec',
+  robinsEggBlue: '#00cec9',
+  firstDate: '#fab1a0',
+  orangeVille: '#e17055',
+  greenDarnerTail: '#74b9ff',
+  electronBlue: '#0984e3',
+  pinkGlamour: '#ff7675',
+  chiGong: '#d63031',
+  shyMoment: '#a29bfe',
+  exodusFruit: '#6c5ce7',
+  pico8Pink: '#fd79a8',
+  prunusAvium: '#e84393',
+  cityLights: '#dfe6e9',
+  soothingBreeze: '#b2bec3',
+  americanRiver: '#636e72',
+  draculaOrchid: '#2d3436',
+};
