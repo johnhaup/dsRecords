fabed43397a6cb7b05ff9ab0b2314b8cab482f6f
John Haupenthal <johnhaup@gmail.com>
2021-07-02 20:24:15 -0700

    Revert to 2.0.0 reanimated.

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/preset-env'],
-  plugins: [
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    // Reanimated plugin has to be listed last.
-    'react-native-reanimated/plugin',
-  ],
-};
