925213d118e49aa3c55cc6f11e6187cd32efe756
John Haupenthal <johnhaup@gmail.com>
2021-07-28 22:00:58 -0700

    Play audio when Bounce is active.

--- src/screens/bounce/Bounce.tsx
+++ src/screens/bounce/Bounce.tsx
@@ -10,18 +10,23 @@ import {
   PanGestureHandlerGestureEvent,
 } from 'react-native-gesture-handler';
 import Animated, {
+  runOnJS,
   useAnimatedGestureHandler,
+  useAnimatedReaction,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
   withDecay,
 } from 'react-native-reanimated';
 import { clamp, withBouncing } from 'react-native-redash';
 import { Record } from '../../components/svgs/Record';
+import { useAudioPlayer } from '../../services/hooks/useAudioPlayer';
 import { colors } from '../../styles';
 
 const RECORD_SIZE = 80;
 
 export const Bounce = () => {
+  const { play, pause } = useAudioPlayer();
   const [containerLayoutRectangle, setContainerLayoutRectangle] =
     useState<LayoutRectangle | null>(null);
 
@@ -30,6 +35,46 @@ export const Bounce = () => {
     (containerLayoutRectangle?.height || RECORD_SIZE) - RECORD_SIZE;
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
+  const activeVelocityX = useSharedValue(0);
+  const activeVelocityY = useSharedValue(0);
+  const decayVelocityX = useSharedValue(0);
+  const decayVelocityY = useSharedValue(0);
+
+  function playFromUI() {
+    'worklet';
+    runOnJS(play)();
+  }
+
+  function pauseFromUI() {
+    'worklet';
+    runOnJS(pause)();
+  }
+
+  const isMoving = useDerivedValue(() => {
+    return (
+      activeVelocityX.value +
+        activeVelocityY.value +
+        decayVelocityX.value +
+        decayVelocityY.value !==
+      0
+    );
+  }, [activeVelocityX, decayVelocityX, activeVelocityY, decayVelocityY]);
+
+  useAnimatedReaction(
+    () => {
+      return isMoving.value;
+    },
+    (isActive, wasActive) => {
+      if (isActive && !wasActive) {
+        playFromUI();
+      }
+      if (!isActive && wasActive) {
+        pauseFromUI();
+      }
+    },
+    [isMoving],
+  );
+
   const onGestureEvent = useAnimatedGestureHandler<
     // Type of event Gesture Handler is emitting
     PanGestureHandlerGestureEvent,
@@ -44,14 +89,26 @@ export const Bounce = () => {
       ctx.offsetY = translateY.value;
     },
     onActive: (event, ctx) => {
+      activeVelocityX.value = event.velocityX;
+      activeVelocityY.value = event.velocityY;
       translateX.value = clamp(ctx.offsetX + event.translationX, 0, boundX);
       translateY.value = clamp(ctx.offsetY + event.translationY, 0, boundY);
     },
     onEnd: ({ velocityX, velocityY }) => {
+      activeVelocityX.value = 0;
+      activeVelocityY.value = 0;
+      decayVelocityX.value = velocityX;
+      decayVelocityY.value = velocityY;
       translateX.value = withBouncing(
-        withDecay({
-          velocity: velocityX,
-        }),
+        withDecay(
+          {
+            velocity: velocityX,
+          },
+          () => {
+            decayVelocityX.value = 0;
+            decayVelocityY.value = 0;
+          },
+        ),
         // Boundaries that will trigger withBouncing hook
         0,
         boundX,
