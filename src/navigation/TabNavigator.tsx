bda645c8ee4cc011f33096f1cbddd70fc0207163
John Haupenthal <johnhaup@gmail.com>
2021-08-15 21:13:41 -0700

    Add tab bar icons. #15

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,16 +1,44 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Record } from '../components/svgs/Record';
 import { Bounce } from '../screens/bounce/Bounce';
 import { Spin } from '../screens/spin/Spin';
+import { colors } from '../styles';
 import { BounceStack, SpinStack } from './types';
 
 const BottomTabNavigator = createBottomTabNavigator();
 
+const renderTabBarIcon = ({ focused }: { focused: boolean }) => {
+  const color = focused ? colors.chiGong : colors.soothingBreeze;
+  const size = focused ? 40 : 36;
+  return <Record size={size} labelColor={color} />;
+};
+
 export const TabNavigator = () => {
   return (
-    <BottomTabNavigator.Navigator>
-      <BottomTabNavigator.Screen name={BounceStack.BOUNCE} component={Bounce} />
-      <BottomTabNavigator.Screen name={SpinStack.SPIN} component={Spin} />
+    <BottomTabNavigator.Navigator
+      tabBarOptions={{ showLabel: false, style: styles.tabBar }}>
+      <BottomTabNavigator.Screen
+        name={BounceStack.BOUNCE}
+        component={Bounce}
+        options={() => ({
+          tabBarIcon: renderTabBarIcon,
+        })}
+      />
+      <BottomTabNavigator.Screen
+        name={SpinStack.SPIN}
+        component={Spin}
+        options={() => ({
+          tabBarIcon: renderTabBarIcon,
+        })}
+      />
     </BottomTabNavigator.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBar: {
+    paddingTop: 8,
+  },
+});
