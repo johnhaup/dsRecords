460441f0cfe88792ff15db580acf2faf5e91eac3
John Haupenthal <johnhaup@gmail.com>
2021-09-04 17:11:13 -0700

    Use one finger to rotate record in Spin.  #32

--- src/screens/spin/Spin.tsx
+++ src/screens/spin/Spin.tsx
@@ -1,62 +1,113 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import {
-  RotationGestureHandler,
-  RotationGestureHandlerGestureEvent,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
 } from 'react-native-gesture-handler';
 import Animated, {
+  runOnJS,
   useAnimatedGestureHandler,
+  useAnimatedReaction,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
-  withDecay,
 } from 'react-native-reanimated';
+import { canvas2Polar } from 'react-native-redash';
+import { AbsoluteContainer } from '../../components/primitives/AbsoluteContainer';
 import { Record } from '../../components/svgs/Record';
+import { useAudioPlayer } from '../../services/hooks/useAudioPlayer';
 import { colors } from '../../styles';
 
-const RECORD_SIZE = 300;
+const RECORD_SIZE = Dimensions.get('window').width - 40;
+const r = RECORD_SIZE / 2;
+const CENTER = { x: 0, y: 0 };
 
 export const Spin = () => {
+  const theta = useSharedValue(0);
   const rotate = useSharedValue(0);
+  const playSpeed = useSharedValue(0);
+  const { play, pause } = useAudioPlayer();
+
+  const isMoving = useDerivedValue(() => playSpeed.value !== 0, [playSpeed]);
+
+  function playFromUI() {
+    'worklet';
+    runOnJS(play)();
+  }
+
+  function pauseFromUI() {
+    'worklet';
+    runOnJS(pause)();
+  }
+
+  useAnimatedReaction(
+    () => isMoving.value,
+    (isActive, wasActive) => {
+      if (isActive && !wasActive) {
+        playFromUI();
+      }
+      if (!isActive && wasActive) {
+        pauseFromUI();
+      }
+    },
+    [isMoving],
+  );
+
   const onGestureEvent = useAnimatedGestureHandler<
-    RotationGestureHandlerGestureEvent,
+    PanGestureHandlerGestureEvent,
     {
       offset: number;
     }
   >({
     onStart: (_, ctx) => {
       ctx.offset = rotate.value;
+      // ctx.offset = polar2Canvas(
+      //   {
+      //     theta: theta.value,
+      //     radius: r,
+      //   },
+      //   CENTER,
+      // );
     },
     onActive: (event, ctx) => {
-      rotate.value = ctx.offset + event.rotation * 1.3;
-    },
-    onEnd: ({ velocity }) => {
-      rotate.value = withDecay({
-        velocity: velocity * 10,
-        deceleration: 0.9999,
-      });
+      const x = event.translationX;
+      const y = event.translationY;
+
+      const value = canvas2Polar({ x, y }, CENTER).theta;
+      theta.value = value > 0 ? value : 2 * Math.PI + value;
+      const thetaRotationValue = theta.value * (360 / (2 * Math.PI)) * -1;
+      const newValue = thetaRotationValue + ctx.offset;
+      rotate.value =
+        newValue > 360 || newValue < -360 ? newValue % 360 : newValue;
+      console.log(ctx.offset, rotate.value);
     },
   });
 
   const style = useAnimatedStyle(() => {
     return {
       transform: [{ rotate: `${rotate.value}deg` }],
+      // transform: [{ translateX: translation.x }, { translateY: translation.y }],
     };
   });
 
   return (
-    <View style={styles.container}>
-      <RotationGestureHandler onGestureEvent={onGestureEvent}>
-        <Animated.View style={style}>
-          <Record size={RECORD_SIZE} labelColor={colors.sourLemon} />
+    <AbsoluteContainer center>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={styles.handlerChild}>
+          <Animated.View style={style}>
+            <Record size={RECORD_SIZE} labelColor={colors.sourLemon} />
+          </Animated.View>
         </Animated.View>
-      </RotationGestureHandler>
-    </View>
+      </PanGestureHandler>
+    </AbsoluteContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  handlerChild: {
+    height: RECORD_SIZE,
+    width: RECORD_SIZE,
+    borderWidth: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
