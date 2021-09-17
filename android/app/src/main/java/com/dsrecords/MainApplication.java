a5b15dafb746805c288feeef4cb8f246d6553ec8
John Haupenthal <johnhaup@gmail.com>
2021-09-16 20:56:16 -0700

    Fix Android build errors.

--- android/app/src/main/java/com/dsrecords/MainApplication.java
+++ android/app/src/main/java/com/dsrecords/MainApplication.java
@@ -1,5 +1,7 @@
 package com.haup.dsrecords;
 
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -10,8 +12,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -37,7 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
         }
 
         @Override
-        protected JSIModulePackage getJSMainModuleName() {
+        protected JSIModulePackage getJSIModulePackage() {
           return new ReanimatedJSIModulePackage();
         }
       };
